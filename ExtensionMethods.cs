c0f26b4fc30a1fe763da7d69ae4a755207173c1d
gnivler <gnivil@gmail.com>
2024-08-22 19:35:34 -0600

    Initial Commit

---
+++ ExtensionMethods.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace ProfitSliders
+{
+    public static class ExtensionMethods
+    {
+        // LLM written
+        public static Transform FindDescendant(this Transform transform, string name)
+        {
+            if (transform.name == name)
+                return transform;
+
+            foreach (Transform child in transform)
+            {
+                var result = child.FindDescendant(name);
+                if (result)
+                    return result;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,17 @@
+﻿using BepInEx;
+using HarmonyLib;
+using HarmonyLib.Tools;
+
+namespace ProfitSliders;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class ProfitSliders : BaseUnityPlugin
+{
+    private void Awake()
+    {
+        // Plugin startup logic
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        new Harmony("DustTradeSliders").PatchAll(typeof(Patches));
+        HarmonyFileLog.Enabled = false;
+    }
+}
